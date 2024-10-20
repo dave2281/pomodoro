d141dfa0996d251346a2a59ba3367a8017a7c28d
David <82700142+dave2281@users.noreply.github.com>
2024-10-20 21:05:30 +0300

    fixed

--- main.rb
+++ main.rb
@@ -1,22 +1,28 @@
 require_relative 'pomo_class.rb'
 
-attr = {
-  work_time: 25,
-  short_time_out: 5,
-  big_time_out: 15,
-  times: 4,               # how much loops of 1 lesson you need?
-  lessons: 2,             # how much lessons you have?
-}
-
 index = 0
 
-while index != attr[:lessons]
-  attr[:times].times do |i|
-    puts "Lesson #{index}"
-    
-    puts "________________"
-  end
-  index += 1
+if ARGV[0] == '-i'
+  Pomodoro.install_libs
 end
 
-Pomodoro.play_audio
+while index != Pomodoro.loop_times
+  Pomodoro.work_time
+  Pomodoro.play_audio
+  Pomodoro.short_time_out
+  Pomodoro.play_audio
+  Pomodoro.work_time
+  Pomodoro.play_audio
+  Pomodoro.short_time_out
+  Pomodoro.play_audio
+  Pomodoro.work_time
+  Pomodoro.play_audio
+  Pomodoro.short_time_out
+  Pomodoro.play_audio
+  Pomodoro.work_time
+  Pomodoro.play_audio
+  Pomodoro.big_time_out
+  Pomodoro.play_audio
+
+  index += 1
+end
--- pomo_class.rb
+++ pomo_class.rb
@@ -1,5 +1,11 @@
 class Pomodoro
   @file = "D:\\pomodoro\\attachments\\budilnik.mp3"
+  @attr = {
+    work_time: 0.1,
+    short_time_out: 0.1,
+    big_time_out: 0.1,
+    times: 4                # how much loops of 1 lesson you need?
+  }
 
   def self.install_libs
     system("sudo apt update")
@@ -7,26 +13,30 @@ class Pomodoro
     system("ffplay -version")
   end
 
+  def self.loop_times
+    @attr[:times]
+  end
+
   def self.play_audio
     puts @file
     system("ffplay -autoexit -nodisp #{@file}")
   end  
 
-  def self.work_time(time)
+  def self.work_time
     puts 'If you want to start work time type Enter:'
     answer = gets
-    sleep(time)
+    sleep(@attr[:work_time]*60)
   end
 
-  def self.short_time_out(time)
+  def self.short_time_out
     puts 'If you want to start short time out, type Enter:'
     answer = gets
-    sleep(time)
+    sleep(@attr[:short_time_out]*60)
   end
 
-  def self.big_time_out(time)
+  def self.big_time_out
     puts 'If you want to start big time out, type Enter:'
     answer = gets
-    sleep(time)
+    sleep(@attr[:big_time_out]*60)
   end
 end
